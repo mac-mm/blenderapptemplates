e6b31d5c3552167ad1c0cc0acc8f08da09bcfadd
mac-mm <paul.mcdonald@gmail.com>
2020-11-17 10:34:41 -0800

    Simplify path discovery and creation for app templates using internal blender functions.

--- src/app_templates.py
+++ src/app_templates.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Create App Template",
     "blender": (2, 80, 0),
-    "category": "Import/Export",
+    "category": "Import-Export",
 }
 
 import os
@@ -12,15 +12,16 @@ import bpy
 
 class WM_saveAppTemplate(bpy.types.Operator):
     """Saves the current layout as a new app template."""
-    bl_label = "Save App Template"
+    bl_label = "Create an app template using the current blend file."
     bl_idname = "wm.saveapptemplate"
-    template_name = bpy.props.StringProperty(name='Template Name', default='New Template')
+    template_name = bpy.props.StringProperty(name='Template Name',
+                                             default='My Template')
 
     def execute(self, context):
         # Get path to save app template to.
-        base_path = self.getPath();
+        base_path =  os.path.join(bpy.utils.resource_path('USER'), 'scripts', 'startup', 'bl_app_templates_user')
         new_template_name = self.template_name
-        app_template_dir = base_path + '/%s' % new_template_name
+        app_template_dir = os.path.join(base_path, new_template_name)
         # Check if directory exists, if not create it.
         Path(app_template_dir).mkdir(parents=True, exist_ok=True)
         bpy.ops.wm.save_as_mainfile(filepath=app_template_dir+'/startup.blend')
@@ -28,21 +29,6 @@ class WM_saveAppTemplate(bpy.types.Operator):
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
-
-    def getPath(self):
-        operating_system = platform.uname().system
-        logged_in_user = pwd.getpwuid(os.getuid()).pw_name
-        blender_version = bpy.app.version
-        # Check if user app templates directory exists, if not create it.
-        if operating_system == 'Darwin':
-            user_templates_dir = '/Users/%s/Library/ApplicationSupport/Blender/%s.%s/scripts/startup/bl_app_templates_user' % (logged_in_user, blender_version[0], blender_version[1])
-        elif operating_system == 'Linux':
-            home = str(Path.home())
-            user_templates_dir = home + '/.config/blender/%s.%s/scripts/startup/bl_app_templates_user' % (blender_version[0], blender_version[1])
-        else:
-            user_templates_dir = '\\Users\\%s\\AppData\\Roaming\\Blender Foundation\\Blender\\%s.%s\\scripts\\startup\\bl_app_templates_user' % (logged_in_user, blender_version[0], blender_version[1])
-        Path(user_templates_dir).mkdir(parents=True, exist_ok=True)
-        return user_templates_dir
 
 def draw_menu(self, context):
     self.layout.separator()
